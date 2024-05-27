57eccb27203fc773bc55928f2b9c1b5371035e75
Maksoit <maksim200425@yandex.ru>
2024-05-27 18:00:48 +0300

    it s working in a bad way

--- core/handlers/callback.py
+++ core/handlers/callback.py
@@ -1,12 +1,14 @@
+import asyncio
 from aiogram import Bot
 from aiogram.types import Message
 from aiogram.types import CallbackQuery
 from core.keyboards.reply import create_pagination_keyboard, do_you_like, get_Math_Prog, get_category_of_math, get_category_of_prog
 from lexicon.lexicon import LEXICON, LEXICON_FOR_FUNC, LEXICON_FOR_SUBJECTS
+from core.utils.dbconnect import Request
 
 
-async def category_search(category: str):
-    return (f'Тут вызвалась функция cо списком предметов категории {category}')
+async def category_search(category: str, request: Request):
+    return await request.category_search(category)
 
 
 # Выбор математики
@@ -20,43 +22,73 @@ async def get_back_math(call: CallbackQuery):
     await call.message.answer(LEXICON_FOR_SUBJECTS[call.data], reply_markup=get_Math_Prog())
 
 # Выбор подкатегории
-async def get_matan(call: CallbackQuery):
+async def get_matan(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
-async def get_linal(call: CallbackQuery):
+async def get_linal(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
-                              reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
-    await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
-
-
-async def get_discra(call: CallbackQuery):
-    await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
-                              reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
-    await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
-
-
-async def get_git(call: CallbackQuery):
-    await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
 
-async def get_diffur(call: CallbackQuery):
+async def get_discra(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
 
-async def get_twims(call: CallbackQuery):
+async def get_git(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
+                              reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
+    await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
+
+
+async def get_diffur(call: CallbackQuery, request: Request):
+    await call.answer()
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
+                              reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
+    await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
+
+
+async def get_twims(call: CallbackQuery, request: Request):
+    await call.answer()
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
@@ -72,29 +104,49 @@ async def get_back_prog(call: CallbackQuery):
     await call.message.answer(LEXICON_FOR_SUBJECTS[call.data], reply_markup=get_Math_Prog())
 
 # Выбор подкатегории
-async def get_C_plusplus(call: CallbackQuery):
+async def get_C_plusplus(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
 
-async def get_C_sharp(call: CallbackQuery):
+async def get_C_sharp(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
 
-async def get_python(call: CallbackQuery):
+async def get_python(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
 
 
-async def get_java(call: CallbackQuery):
+async def get_java(call: CallbackQuery, request: Request):
     await call.answer()
-    await call.message.answer(await category_search(category=LEXICON_FOR_FUNC[call.data]),
+    data = await category_search(category=LEXICON_FOR_FUNC[call.data], request=request)
+    print(data)
+    text = ''
+    for i in range(len(data)):
+        text += f"{i+1}. {data[i]['name']}, {data[i]['authors']}\n"
+    await call.message.answer(text,
                               reply_markup=create_pagination_keyboard('backward','какая страница/из скольки','forward'))
     await call.message.answer(LEXICON['text_likes'], reply_markup=do_you_like())
