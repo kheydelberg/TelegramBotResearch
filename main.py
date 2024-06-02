b43f312d8bdaa918b5f8ffa94b6c5fb9cbc2310d
Maksoit <maksim200425@yandex.ru>
2024-06-02 22:29:48 +0300

    as;dfj

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import codecs
+
+from aiogram.types import User
 from core.utils.dbconnect import Request
 from typing import Text
 from aiogram import Bot, Dispatcher
 from core.handlers.basic import get_choose_subject, get_help, get_start, pagination
 import asyncio
 import logging
-
+from core.state.user import UserState
 import aiomysql
 
 from aiogram.fsm.storage.redis import RedisStorage
@@ -20,7 +22,7 @@ from aiogram import F
 from core.utils.commands import set_commands
 
 from aiogram.filters import Command
-from core.handlers.callback import book_callback_handler, get_C_plusplus, get_C_sharp, get_back_math, get_back_prog, get_diffur, next_page_callback_handler, prev_page_callback_handler
+from core.handlers.callback import book_callback_handler, get_C_plusplus, get_C_sharp, get_back_math, get_back_prog, get_choose_subject2, get_diffur, like, next_page_callback_handler, notlike, prev_page_callback_handler
 from core.handlers.callback import get_discra, get_git, get_java, get_linal, get_matan, get_python, get_twims
 from core.handlers.callback import get_math, get_prog
 
@@ -120,23 +122,29 @@ async def start():
     
     dp.callback_query.register(get_prog, F.data == 'prog')
     dp.callback_query.register(get_math, F.data == 'math')
-    dp.callback_query.register(get_matan, F.data == 'matan')
-    dp.callback_query.register(get_linal, F.data == 'linal')
-    dp.callback_query.register(get_discra, F.data == 'discra')
-    dp.callback_query.register(get_git, F.data == 'git')
-    dp.callback_query.register(get_diffur, F.data == 'diffur')
-    dp.callback_query.register(get_twims, F.data == 'twims')
+    
+    
+    # dp.callback_query.register(get_linal, F.data == 'linal')
+    # dp.callback_query.register(get_discra, F.data == 'discra')
+    # dp.callback_query.register(get_git, F.data == 'git')
+    # dp.callback_query.register(get_diffur, F.data == 'diffur')
+    # dp.callback_query.register(get_twims, F.data == 'twims')
 
-    dp.callback_query.register(get_C_plusplus, F.data == 'C++')
-    dp.callback_query.register(get_C_sharp, F.data == 'C#')
-    dp.callback_query.register(get_python, F.data == 'python')
-    dp.callback_query.register(get_java, F.data == 'java')
+    # dp.callback_query.register(get_C_plusplus, F.data == 'C++')
+    # dp.callback_query.register(get_C_sharp, F.data == 'C#')
+    # dp.callback_query.register(get_python, F.data == 'python')
+    # dp.callback_query.register(get_java, F.data == 'java')
     dp.callback_query.register(get_back_math, F.data == 'back_math')
     dp.callback_query.register(get_back_prog, F.data == 'back_prog')
 
     dp.callback_query.register(book_callback_handler, F.data.startswith("book_"))
     dp.callback_query.register(prev_page_callback_handler, F.data == 'prev_page')
     dp.callback_query.register(next_page_callback_handler, F.data == 'next_page')
+    dp.callback_query.register(get_choose_subject2, F.data =='back_to_choice')
+    dp.callback_query.register(like, F.data == 'like')
+    dp.callback_query.register(notlike, F.data == 'not_like')
+    dp.callback_query.register(get_matan, UserState.PICK_LINK)
+    
 
     dp.message.register(pagination, Command(commands='test_pagination'))
     dp.message.register(get_choose_subject, Command(commands='subject'))
